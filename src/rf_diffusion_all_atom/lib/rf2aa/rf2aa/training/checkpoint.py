31b3457faa1ade33dc47496ff6529b4243a07c6c
Yakubov S <35y@${HOSTNAME}.ornl.gov>
2025-08-31 20:28:17 -0400

    refactor to package, add pixi config

--- src/rf_diffusion_all_atom/lib/rf2aa/rf2aa/training/checkpoint.py
+++
@@ -1,5 +0,0 @@
-# for gradient checkpointing
-def create_custom_forward(module, **kwargs):
-    def custom_forward(*inputs):
-        return module(*inputs, **kwargs)
-    return custom_forward
--- src/rf_diffusion_all_atom/mask_generator.py
+++
@@ -1,4 +0,0 @@
-def get_triple_contact():
-    pass
-def get_diffusion_mask_simple():
-    pass
